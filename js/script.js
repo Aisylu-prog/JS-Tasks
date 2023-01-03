f4f1f69255f69dd67d85c1c9966b9f2db2ce3afe
Aisylu <ice_yoona@mail.ru>
2023-01-03 19:18:53 +0100

    2 part

--- js/script.js
+++ js/script.js
@@ -1,22 +1,75 @@
 'use strict';
 
-const numberOfMovies = +prompt('How much movies have you watch yet?', '');
+
+const numberOfFilms = +prompt('Сколько фильмов за год вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count:numberOfMovies,
+    count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
+    genres: [],
     privat: false
 };
 
-const a = prompt('The last movie you watched?',''),
-      b = prompt('How do you rate this movie?',''),
-      c = prompt('The last movie you watched?',''),
-      d = prompt('How do you rate this movie?','');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;      
-personalMovieDB.movies[c] = d;  
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
+
+function sayHello(name) {
+    return `Hi, ${name}!)`;
+}
+
+console.log(sayHello('Anna'));
+
+
+function returnNeighboringNumbers(num) {
+    return [num-1, num, num+1];
+}
+
+console.log(returnNeighboringNumbers(5));
+
+function getMathResult(num, times) {
+    if ((typeof(times) == 'number') && (times > 0)) {
+
+        let str = '';
+        for (let i = 1; i <= times; i++) {
+            if (i === times) {
+                str += `${num * i}`;
+            } else {
+                str += `${num * i}---`;
+            }
+        }
+
+        return str;
+
+    } else {
+
+        return num;
+
+    }
+
+}
+
+console.log(getMathResult(5, 5));
+
