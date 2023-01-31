01deeb0b312d024e734a114898acbd94b82d09a1
Aisylu <ice_yoona@mail.ru>
2023-01-31 15:45:37 +0100

    fourth

---
+++ js/task 2 cycle.js
@@ -0,0 +1,18 @@
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    for (let i = 0; i < data.length; i++ ) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data [i] * 2;
+        } else {
+            data[i] = '${data[i]} done';
+        }
+    }
+
+
+    console.log(data);
+
+    return data;
+}
+
+secondTask();
